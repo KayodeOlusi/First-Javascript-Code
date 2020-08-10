c20faf4a8c0f6dc3a676867b361c51ad66aa0593
KayodeOlusi <66219016+KayodeOlusi@users.noreply.github.com>
2020-08-10 18:02:53 +0000

    Update CardGame.js

--- CardGame.js
+++ CardGame.js
@@ -1,1 +1,38 @@
 # Card Game
+//Card Game
+
+var count=0;
+
+function myFunc(card){
+switch(card){
+ case 2:
+ case 3:
+ case 4:
+ case 5:
+ case 6:
+count++;
+break;
+ case "a":
+ case "q":
+ case 10:
+ case "j":
+count--;
+break;
+}
+
+var holdbet="hold";
+if(count>0){
+holdbet="bet"
+}
+
+return count + " " + holdbet;
+
+}
+myFunc("j");
+myFunc(2);
+myFunc("a");
+myFunc(10);
+myFunc(6);
+myFunc(4);
+
+//End Card Game
